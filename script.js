bb7126cdd4a48886ba7e009782917a9765cacdeb
Kian Bruyenberg <56255204+TheTrumpetist27@users.noreply.github.com>
2022-10-27 17:07:25 +0200

    Removed personel notes

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
 "use strict";
 
-/*
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "🎉 Correct Number!";
-
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value);
-*/
-
-// addEventListener() = om een event op de webpagina af te wachten.
-// Het eerste argument is welk event we aan het checken zijn.
-// Het 2e argument is de functie die uitgevoerd moet worden als het event heeft plaatsgevonden.
-
-// const x = function() {
-//     console.log(23);
-// }
-
-// Een heel random getal
+// a random int
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
